acb6cc2af3383a32d99bb88cbac415c579737d59
mahdi-khoshkhabar <mahdikhoshkhabar1382@gmail.com>
2025-09-02 16:43:43 +0330

    Add calculator functionality and corresponding tests
    - Implemented basic calculator operations: addition, subtraction, multiplication, division, modulus, and exponentiation.
    - Added validation functions for operators and numbers.
    - Created unit tests for checkNumber, checkOperator, and calcTwoNumbers functions.
    - Set up HTML file to run tests using Mocha and Chai.

---
+++ scripts/calculator.js
@@ -0,0 +1,79 @@
+"use strict";
+console.log("start app");
+
+function checkOperator(operator) {
+  switch (operator) {
+    case "+":
+    case "-":
+    case "/":
+    case "*":
+    case "**":
+    case "%":
+      console.log("true operator");
+      return true;
+
+    default:
+      //   alert("wrong operator");
+      console.log("wrong operator: ", operator);
+      return false;
+  }
+}
+
+function checkNumber(num) {
+  if (typeof num === "number") {
+    // console.log()
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function calcTwoNumbers(num1, num2, operator) {
+  switch (operator) {
+    case "+":
+      return num1 + num2;
+
+    case "-":
+      return num1 - num2;
+
+    case "*":
+      return num1 * num2;
+
+    case "/":
+      return num1 / num2;
+
+    case "%":
+      return num1 % num2;
+
+    case "**":
+      return num1 ** num2;
+
+    default:
+      console.log("calculator: wrong format");
+      break;
+  }
+}
+
+let num1 = +prompt("number?");
+let operator;
+let num2;
+
+do {
+  operator = prompt("operator?");
+
+  if (!checkOperator(operator)) {
+    break;
+  }
+
+  num2 = prompt("number?") == "" ? null : +prompt("number?");
+
+  if (checkNumber(num1) && checkNumber(num2)) {
+    num1 = calcTwoNumbers(num1, num2, operator);
+  } else {
+    break;
+  }
+} while (true);
+
+alert(num1);
+
+console.log("end app");
---
+++ scripts/test.js
@@ -0,0 +1,89 @@
+describe("checkNumber function", function () {
+  it("numbers inputs returns true", function () {
+    assert.equal(checkNumber(1), true);
+
+    assert.equal(checkNumber(0), true);
+
+    assert.equal(checkNumber(-1), true);
+
+    assert.equal(checkNumber(+"1"), true);
+
+    assert.equal(checkNumber(+"0"), true);
+
+    assert.equal(checkNumber(+"-1"), true);
+  });
+
+  it("other inputs returns false", function () {
+    assert.equal(checkNumber("text"), false);
+
+    assert.equal(checkNumber("+"), false);
+
+    assert.equal(checkNumber("="), false);
+
+    assert.equal(checkNumber(null), false);
+
+    assert.equal(checkNumber(undefined), false);
+  });
+});
+
+describe("checkOperator function", function () {
+  it("operator inputs returns true", function () {
+    assert.equal(checkOperator("+"), true);
+
+    assert.equal(checkOperator("-"), true);
+
+    assert.equal(checkOperator("/"), true);
+
+    assert.equal(checkOperator("*"), true);
+
+    assert.equal(checkOperator("%"), true);
+
+    assert.equal(checkOperator("**"), true);
+  });
+
+  it("other inputs returns false", function () {
+    assert.equal(checkOperator(null), false);
+
+    assert.equal(checkOperator(undefined), false);
+  });
+});
+
+describe("calcTwoNumbers function", function () {
+  it("+", function () {
+    assert.equal(calcTwoNumbers(2, 3, "+"), 5);
+    assert.equal(calcTwoNumbers(0, 3, "+"), 3);
+    assert.equal(calcTwoNumbers(-2, 2, "+"), 0);
+  });
+
+  it("-", function () {
+    assert.equal(calcTwoNumbers(2, 3, "-"), -1);
+    assert.equal(calcTwoNumbers(3, 2, "-"), 1);
+    assert.equal(calcTwoNumbers(3, 3, "-"), 0);
+    assert.equal(calcTwoNumbers(0, 3, "-"), -3);
+  });
+
+  it("/", function () {
+    assert.equal(calcTwoNumbers(6, 2, "/"), 3);
+    assert.equal(calcTwoNumbers(2, 2, "/"), 1);
+    assert.equal(calcTwoNumbers(1, 2, "/"), 0.5);
+    // TODO: add conditions for [number]/0 and etc.
+  });
+
+  it("%", function () {
+    assert.equal(calcTwoNumbers(4, 2, "%"), 0);
+    assert.equal(calcTwoNumbers(5, 2, "%"), 1);
+  });
+
+  it("*", function () {
+    assert.equal(calcTwoNumbers(2, 3, "*"), 6);
+    assert.equal(calcTwoNumbers(9, 0, "*"), 0);
+    assert.equal(calcTwoNumbers(-3, 4, "*"), -12);
+    assert.equal(calcTwoNumbers(0, 0, "*"), 0);
+  });
+
+  it("**", function () {
+    assert.equal(calcTwoNumbers(2, 3, "**"), 8);
+    assert.equal(calcTwoNumbers(3, 3, "**"), 27);
+    assert.equal(calcTwoNumbers(9, 0, "**"), 1);
+  });
+});
